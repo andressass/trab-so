bc549c7121c6fcb142ed4e53dcc7e5480167614d
Andressa Sousa da Silveira <andressa.s.silveira@gmail.com>
2016-05-18 09:05:39 -0300

    Implementação do semáforo. Obs.: Teste do semáforo dando ERRADO!

---
+++ ServidorMemoria/ServidorMemoria/include/I_SEMAFORO.h
@@ -0,0 +1,13 @@
+//
+//  SEMAFORO.h
+//  ServidorMemoria
+//
+//  Created by Andressa Sousa da Silveira on 17.05.16.
+//  Copyright © 2016 Andressa Silveira e Rondinele Prado. All rights reserved.
+//
+
+#ifndef SEMAFORO_h
+#define SEMAFORO_h
+
+
+#endif /* SEMAFORO_h */
---
+++ ServidorMemoria/ServidorMemoria/src/M_SEMAFORO.c
@@ -0,0 +1,9 @@
+//
+//  M_SEMAFORO.c
+//  ServidorMemoria
+//
+//  Created by Andressa Sousa da Silveira on 17.05.16.
+//  Copyright © 2016 Andressa Silveira e Rondinele Prado. All rights reserved.
+//
+
+#include "M_SEMAFORO.h"
